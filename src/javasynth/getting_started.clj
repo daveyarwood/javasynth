925152a68c77fdb39b67a26658ada4dc65767b3f
Dave Yarwood <dave.yarwood@gmail.com>
2016-08-21 15:55:40 -0400

    amplitude

--- src/javasynth/getting_started.clj
+++ src/javasynth/getting_started.clj
@@ -2,10 +2,11 @@
   (:import [com.jsyn JSyn]
            [com.jsyn.unitgen LineOut SineOscillator]))
 
-(defn play-sine [freq dur]
+(defn play-sine [freq dur amp]
   (let [synth (doto (. JSyn createSynthesizer) .start)
         out   (LineOut.)
         sine  (SineOscillator. freq)]
+    (.set (. sine -amplitude) amp)
     (.add synth out)
     (.add synth sine)
     (.connect (. sine -output) (. out -input))
