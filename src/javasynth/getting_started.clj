3a4d28f178d30fc77e745f8d8f59488eb288af76
Dave Yarwood <dave.yarwood@gmail.com>
2017-12-02 22:26:20 -0500

    LinearRamp noodling

--- src/javasynth/getting_started.clj
+++ src/javasynth/getting_started.clj
@@ -1,16 +1,22 @@
 (ns javasynth.getting-started
   (:import [com.jsyn JSyn]
-           [com.jsyn.unitgen LineOut SineOscillator]))
+           [com.jsyn.unitgen LineOut SineOscillator LinearRamp]))
 
 (defn play-sine [freq dur amp]
   (let [synth (doto (. JSyn createSynthesizer) .start)
         out   (LineOut.)
+        ramp  (LinearRamp.)
         sine  (SineOscillator. freq)]
-    (.set (. sine -amplitude) amp)
+    (.set (. ramp -current) amp)
+    (.set (. ramp -input) amp)
+    (.set (. ramp -time) (/ dur 1000))
     (.add synth out)
+    (.add synth ramp)
     (.add synth sine)
     (.connect (. sine -output) (. out -input))
     (let [now (. synth getCurrentTime)]
       (.start out)
+      (.start ramp)
+      (. synth (sleepUntil (+ now 0.01)))
       (. synth (sleepUntil (+ now (/ dur 1000))))
       (.stop synth))))
